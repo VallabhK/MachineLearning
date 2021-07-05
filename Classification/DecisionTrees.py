2b8359312a18fcb7f378f937838310a43086f58c
Vallabh Karanjkar <vallabhkaranjkar@outlook.com>
2021-07-05 23:13:25 +0530

    Added main function to test the functionality

--- Classification/DecisionTrees.py
+++ Classification/DecisionTrees.py
@@ -116,3 +116,26 @@ class DecisionTree:
         counter = Counter(y)
         most_common = counter.most_common(1)[0][0]
         return most_common
+if __name__ == "__main__":
+    # Imports
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    data = datasets.load_breast_cancer()
+    X, y = data.data, data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    clf = DecisionTree(max_depth=10)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    acc = accuracy(y_test, y_pred)
+
+    print("Accuracy:", acc)
