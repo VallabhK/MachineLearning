e5447cee490f4ac70c4149cce8065ecaf0a8a1f7
Vallabh Karanjkar <vallabhkaranjkar@outlook.com>
2021-07-10 21:12:43 +0530

    Update RandomForest.py

--- Classification/RandomForest.py
+++ Classification/RandomForest.py
@@ -1,1 +1,13 @@
 #This file contains the implementation of Random Forest algorithm
+
+#Import all the required packages
+from collections import Counter
+import numpy as np
+
+#Import the decision tree implemented previously
+from .decision_tree import DecisionTree
+
+def bootstrap_sample(X, y):
+    n_samples = X.shape[0]
+    idxs = np.random.choice(n_samples, n_samples, replace=True)
+    return X[idxs], y[idxs]
