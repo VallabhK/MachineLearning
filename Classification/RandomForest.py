d7bc21fcae6f9df04ac7237ff3a1559b6fca1280
VallabhK <vallabhkaranjkar@outlook.com>
2021-07-13 19:47:25 +0530

    Added test functionality for Random Forest algorithm

--- Classification/RandomForest.py
+++ Classification/RandomForest.py
@@ -44,3 +44,28 @@ class RandomForest:
         y_pred = [most_common_label(tree_pred) for tree_pred in tree_preds]
         return np.array(y_pred)
     
+
+if __name__ == "__main__":
+    # Include all the packages
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    clf = RandomForest(n_trees=3, max_depth=10)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy(y_test, y_pred)
+
+    print("Accuracy:", acc)
